c1fb20a08e37b8817d2d4aba1dbf9821a0038dce
jgaffuri <jgaf@proton.me>
2024-06-16 19:47:56 +0200

    a

--- bv/src/bv_points.py
+++ bv/src/bv_points.py
@@ -4,31 +4,35 @@ from shapely.geometry import MultiPolygon, Polygon
 
 
 def convert_multipolygon_to_centroid(input_gpkg, output_gpkg):
+    new_features = []
+
     with fiona.open(input_gpkg, layer=0) as source:
         schema = source.schema.copy()
         schema['geometry'] = 'Point'
 
-        with fiona.open(output_gpkg, 'w', driver='GPKG', crs=source.crs, schema=schema) as out_gpkg:
-            for feature in source:
-                geom = shape(feature['geometry'])
-                
-                if isinstance(geom, MultiPolygon):
-                    largest_polygon = max(geom.geoms, key=lambda a: a.area)
-                elif isinstance(geom, Polygon):
-                    largest_polygon = geom
-                else:
-                    print("Unexpected geometry type: " + feature['geometry']['type'])
-                    continue
+        for feature in source:
+            geom = shape(feature['geometry'])
+            
+            if isinstance(geom, MultiPolygon):
+                largest_polygon = max(geom.geoms, key=lambda a: a.area)
+            elif isinstance(geom, Polygon):
+                largest_polygon = geom
+            else:
+                print("Unexpected geometry type: " + feature['geometry']['type'])
+                continue
 
-                centroid = largest_polygon.centroid
-                
-                new_feature = {
-                    'type': 'Feature',
-                    'geometry': mapping(centroid),
-                    'properties': feature['properties']
-                }
-                
-                out_gpkg.write(new_feature)
+            centroid = largest_polygon.centroid
+            
+            new_feature = {
+                'type': 'Feature',
+                'geometry': mapping(centroid),
+                'properties': feature['properties']
+            }
+            
+            new_features.append(new_feature)
+
+    with fiona.open(output_gpkg, 'w', driver='GPKG', crs=source.crs, schema=schema) as sink:
+        sink.writerecords(new_features)
 
 
 folder = "/home/juju/geodata/elections_fr/bv/"
