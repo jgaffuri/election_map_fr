11e3730ac4cc19cb79a2294d3c4ce111b781f050
jgaffuri <jgaf@proton.me>
2024-06-19 00:49:24 +0200

    a

--- eur2024/src/prepare_gridviz.py
+++ eur2024/src/prepare_gridviz.py
@@ -13,7 +13,7 @@ folder = "/home/juju/geodata/elections_fr/eur2024/"
 if format_join:
 
     #load election results
-    df = pd.read_csv(folder + "eur_resultats-definitifs-par-bureau-de-vote.csv", sep=";")
+    df = pd.read_csv(folder + "resultats-definitifs-par-bureau-de-vote.csv", sep=";", dtype={'Code commune': str, 'Code BV': str})
 
     #remove unecessary columns
     df = df.drop(columns=[col for col in df.columns if '%' in col])
@@ -26,12 +26,8 @@ if format_join:
     #rename Voix X -> vX
     for i in range(1,39): df.rename(columns={'Voix '+str(i): 'v'+str(i)}, inplace=True)
 
-    #add column
-    df['nb_bv'] = 1
-
-    #make new field
-    #df['id_bv'] = df['Code commune'].str.cat(df['Code BV'], sep='_')
-    df['id_bv'] = df.apply(lambda row: str(row['Code commune']) + "_" + str(row['Code BV']), axis=1)
+    #make new field codeBureauVote
+    df['codeBureauVote'] = df.apply(lambda row: str(row['Code commune']) + "_" + str(row['Code BV']), axis=1)
     df = df.drop(columns=["Code BV", "Code commune"])
 
     #print(df.iloc[0].to_string())
@@ -39,14 +35,16 @@ if format_join:
 
 
     #load bv data
-    df2 = pd.read_csv("/home/juju/geodata/elections_fr/bv/bv.csv")
-    df2 = df2.drop(columns=["fid","numeroBureauVote","codeBureauVote"])
+    df2 = pd.read_csv("/home/juju/geodata/elections_fr/bv/bv.csv", dtype={'codeCirconscription': str})
+    df2 = df2.drop(columns=["fid","numeroBureauVote","id_bv"])
     #print(df2.iloc[0].to_string())
 
-
     #join
-    df = pd.merge(df2, df, on='id_bv', how='left')
+    df = pd.merge(df2, df, on='codeBureauVote', how='left')
     #print(df.iloc[0].to_string())
+
+    #add column
+    df['nb_bv'] = 1
 
     # Remove rows where 'Inscrits' is NaN, 0 or ""
     df = df[pd.notna(df['Inscrits'])]
@@ -67,7 +65,7 @@ if aggregate:
         "nomCirconscription": gridtiler.aggregation_single_value,
         "codeCommune": gridtiler.aggregation_single_value,
         "nomCommune": gridtiler.aggregation_single_value,
-        "id_bv": gridtiler.aggregation_single_value,
+        "codeBureauVote": gridtiler.aggregation_single_value,
         }
 
     print(datetime.now(), "aggregation to", 100, "m")
@@ -87,7 +85,7 @@ if aggregate:
     for resolution in [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]:
         f = folder+str(resolution)+".csv"
         df = pd.read_csv(f)
-        df.loc[df['nb_bv'] > 1, ['codeDepartement', 'codeCirconscription', 'codeCommune', 'nomCirconscription', 'nomCommune', 'id_bv']] = None
+        df.loc[df['nb_bv'] > 1, ['codeDepartement', 'codeCirconscription', 'codeCommune', 'nomCirconscription', 'nomCommune', 'codeBureauVote']] = None
         df.to_csv(f, index=False)
 
 
