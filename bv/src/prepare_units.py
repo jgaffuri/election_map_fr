63c95aaf587cc611171818209363252cf10cb6ac
jgaffuri <jgaf@proton.me>
2024-06-18 16:23:27 +0200

    a

--- bv/src/prepare_units.py
+++ bv/src/prepare_units.py
@@ -1,8 +1,76 @@
 import geopandas as gpd
-from shapely.geometry import box
-from shapely.geometry import mapping, shape
-import json
+from shapely.geometry import box, Polygon, Point, LineString, MultiPoint, MultiLineString, MultiPolygon, GeometryCollection
 from shapely.validation import make_valid
+
+from bv_points import convert_multipolygon_to_centroid
+
+
+
+def make_polygonal(geom):
+    if isinstance(geom, Polygon):
+        return geom
+    if isinstance(geom, MultiPolygon):
+        return geom
+    if isinstance(geom, GeometryCollection):
+        ps = []
+        for g in geom.geoms:
+            if not isinstance(g, Polygon): continue
+            if g.area == 0: continue
+            ps.append(g)
+        l = len(ps)
+        if l == 0: return Polygon()
+        if l == 1: return ps[0]
+        return MultiPolygon(ps)
+    return Polygon()
+
+
+
+def make_multipolygonal(geom):
+    if isinstance(geom, MultiPolygon):
+        return geom
+    if isinstance(geom, Polygon):
+        return MultiPolygon([geom])
+    if isinstance(geom, GeometryCollection):
+        ps = []
+        for g in geom.geoms:
+            if not isinstance(g, Polygon): continue
+            if g.area == 0: continue
+            ps.append(g)
+        return MultiPolygon(ps)
+    return MultiPolygon()
+
+
+
+
+
+def round_coordinates(geom, precision=3):
+    if isinstance(geom, Point):
+        return Point(round(geom.x, precision), round(geom.y, precision))
+    elif isinstance(geom, LineString):
+        cs = []
+        for c in geom.coords:
+            x, y = c
+            cs.append((round(x, precision), round(y, precision)))
+        return make_valid(LineString(cs))
+    elif isinstance(geom, Polygon):
+        exterior_ring = round_coordinates(geom.exterior, precision)
+        if isinstance(exterior_ring, Point): return exterior_ring
+        if Polygon(exterior_ring).area == 0: return exterior_ring
+        interior_rings = []
+        for ir in geom.interiors:
+            ir = round_coordinates(ir, precision)
+            if isinstance(ir, Point): continue
+            if Polygon(ir).area == 0: continue
+            interior_rings.append(ir)
+        return make_valid(Polygon(exterior_ring, interior_rings))
+    elif isinstance(geom, (MultiPoint, MultiLineString, MultiPolygon, GeometryCollection)):
+        ps = [ round_coordinates(p, precision) for p in geom.geoms ]
+        return make_valid(GeometryCollection(ps))
+
+    print("unsupported geometry type: " + geom.geom_type)
+    return geom
+
+
 
 
 folder = "/home/juju/geodata/elections_fr/circonscriptions/insee/"
@@ -20,26 +88,25 @@ extent = box(90000, 6100000, 1130000, 7140000)
 gdf = gdf.to_crs("EPSG:"+epsg)
 
 #make valid geometries
-gdf['geometry'] = gdf['geometry'].apply(make_valid)
+gdf['geometry'] = gdf['geometry'].apply(lambda geom: geom.buffer(0))
+gdf['geometry'] = gdf['geometry'].apply(lambda geom: make_multipolygonal(geom))
+gdf['geometry'] = gdf['geometry'].apply(lambda geom: geom.buffer(0))
+#gdf['geometry'] = gdf['geometry'].apply(make_valid)
 
 #clip
 gdf = gdf.clip(extent)
 
-#save
-gdf.to_file(terr + '.geojson', driver='GeoJSON')
-
-
-
-
-
+#make valid geometries
+gdf['geometry'] = gdf['geometry'].apply(make_valid)
 
 #apply rounding to each geometry in the GeoDataFrame
 #gdf['geometry'] = gdf['geometry'].apply(lambda geom: round_coordinates(geom, precision=0))
+#gdf['geometry'] = gdf['geometry'].apply(lambda geom: make_multipolygonal(geom))
+#make valid geometries
+#gdf['geometry'] = gdf['geometry'].apply(make_valid)
 
-'''
-def round_coordinates(geom, precision=0):
-    if geom.is_empty:
-        return geom
-    rounded_coords = json.loads(json.dumps(mapping(geom), rounding=precision))
-    return shape(rounded_coords)
-'''
+#save
+gdf.to_file(folder + terr + '.gpkg', driver='GPKG')
+
+#save to points
+convert_multipolygon_to_centroid(folder + terr + '.gpkg', folder + terr + '_pt.gpkg')
