a86f32144d47256b4f9ff2eece30a610af9d701c
jgaffuri <jgaf@proton.me>
2024-06-18 22:15:22 +0200

    a

--- eur2024/src/prepare_gridviz.py
+++ eur2024/src/prepare_gridviz.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from pygridmap import gridtiler
 from datetime import datetime
-
+import os
 
 format_join = False
-aggregate = True
+aggregate = False
+tiling = True
 
 folder = "/home/juju/geodata/elections_fr/eur2024/"
 
 
 if format_join:
+
     #load election results
     in_file = folder + "eur_resultats-definitifs-par-bureau-de-vote.csv"
     df = pd.read_csv(in_file, sep=";")
@@ -20,7 +22,9 @@ if format_join:
     df = df.drop(columns=[col for col in df.columns if 'Numéro de panneau' in col])
     df = df.drop(columns=[col for col in df.columns if 'Libellé' in col])
     df = df.drop(columns=[col for col in df.columns if 'Nuance' in col])
-    df = df.drop(columns=["Code localisation", "Code département"])
+    df = df.drop(columns=["Code localisation", "Code département", "Votants", "Abstentions", "Exprimés"])
+
+
 
 
     #make new field
@@ -46,6 +50,7 @@ if format_join:
     join.to_csv(folder + "1.csv", index=False)
 
 
+#aggregation
 if aggregate:
 
     aggregation_fun = {
@@ -75,3 +80,24 @@ if aggregate:
     for a in [2,5,10]:
         print(datetime.now(), "aggregation to", a*10000, "m")
         gridtiler.grid_aggregation(input_file=folder+"10000.csv", resolution=1000, output_file=folder+str(a*10000)+".csv", a=a)
+
+
+#tiling
+if tiling:
+    for resolution in [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]:
+        print("tiling for resolution", resolution)
+
+        #create output folder
+        out_folder = 'pub/gridviz/tiles/' + str(resolution)
+        if not os.path.exists(folder): os.makedirs(folder)
+
+        gridtiler.grid_tiling(
+            folder+str(resolution)+".csv",
+            out_folder,
+            resolution,
+            tile_size_cell = 256,
+            x_origin = 0,
+            y_origin = 0,
+            #crs = "EPSG:3035",
+            format = "parquet"
+        )
