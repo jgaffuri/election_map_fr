cafc3dd19db01712382b43b7994525b02566cf3f
jgaffuri <jgaf@proton.me>
2024-06-30 15:35:01 +0200

    a

---
+++ units/src/leg2024/resultats_circo_T1.py
@@ -0,0 +1,12 @@
+import requests
+import xml.etree.ElementTree as ET
+import csv
+
+
+folder = "tmp/"
+url = "https://www.resultats-elections.interieur.gouv.fr/telechargements/LG2024/nuances/nuances.xml"
+#xml_file = folder + "nuances.xml"
+csv_file= folder + "resultats_circos_T1.csv"
+
+
+
