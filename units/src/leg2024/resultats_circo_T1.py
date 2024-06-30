45cf44ffbef0462c7f672d279df395e18659d0d0
jgaffuri <jgaf@proton.me>
2024-06-30 21:51:30 +0200

    a

--- units/src/leg2024/resultats_circo_T1.py
+++ units/src/leg2024/resultats_circo_T1.py
@@ -1,12 +1,68 @@
-import requests
 import xml.etree.ElementTree as ET
 import csv
 
-
 folder = "tmp/"
-url = "https://www.resultats-elections.interieur.gouv.fr/telechargements/LG2024/nuances/nuances.xml"
-#xml_file = folder + "nuances.xml"
-csv_file= folder + "resultats_circos_T1.csv"
+file_code = "R125"
+
+
+def xml_to_csv(xml_file_path, csv_file_path):
+    # Parse the XML data from the file
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
+
+    # Create a list to hold the rows for the CSV
+    rows = []
+    
+    for circo in root.findall('.//Circonscriptions/Circonscription'):
+        cod_cir_elec = circo.find('.//CodCirElec').text
+        print("*** "+cod_cir_elec)
+
+        tour = circo.find('.//Tours/Tour')
+
+        inscrits_number = tour.find('.//Mentions/Inscrits/Nombre').text
+
+        for candidat in tour.findall('.//Resultats/Candidats/Candidat'):
+            print(candidat)
+
+            nom_psn = candidat.find('NomPsn').text if candidat.find('NomPsn') is not None else ''
+            prenom_psn = candidat.find('PrenomPsn').text if candidat.find('PrenomPsn') is not None else ''
+            civilite_psn = candidat.find('CivilitePsn').text if candidat.find('CivilitePsn') is not None else ''
+            cod_nua_cand = candidat.find('CodNuaCand').text if candidat.find('CodNuaCand') is not None else ''
+            nb_voix = candidat.find('NbVoix').text if candidat.find('NbVoix') is not None else ''
+            elu = candidat.find('Elu').text if candidat.find('Elu') is not None else ''
+
+            print(nom_psn, prenom_psn)
+
+            # Append the row to the list
+            rows.append([
+                cod_cir_elec,
+                inscrits_number,
+                nom_psn,
+                prenom_psn,
+                civilite_psn,
+                cod_nua_cand,
+                nb_voix,
+                elu,
+            ])
+
+    # Define the CSV headers
+    headers = [
+        'NomPsn',
+        'PrenomPsn',
+        'CivilitePsn',
+        'CodNuaCand',
+        'NbVoix',
+        'Elu',
+        'CodCirElec',
+        'Inscrits'
+    ]
+    
+    # Write the rows to a CSV file
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(rows)
 
 
 
+xml_to_csv("tmp/download_circo/"+file_code+"CIR.xml", "tmp/"+file_code+"CIR.csv")
